018b6b8bda43ce088316ce1939fc94e6cc8eaf6e
Bernd Storck <berndstorck@hotmail.com>
2020-03-23 23:05:40 +0100

    Version 3.5.0. Was changed to work correct with the current database format of 'dict.cc'. 2. Extracts now more data sets from the database of 'dict.cc'.

--- mkbase.sh
+++ mkbase.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #
-# mkbase.sh 1.1.0
+# mkbase.sh 1.3.0  (2020-03-21)
 #
 # Erzeugt eine vereinfachte, kuerzere Datendatei aus der Woerterbuchdatei von dict.cc.
 # 
@@ -33,7 +33,7 @@ if [ "$1" ]; then
 	esac
 
 else
-	dict_archive="$(ls -A1 cmc*.zip)"
+	dict_archive="$(ls -A1 cm*.zip)"
 
 fi 
 
@@ -52,8 +52,10 @@ if [ "$dict_archive" ]; then
 
 		if [ "$inputfile" ]; then
 
-			grep --color=never -E "[[:blank:]]verb$" "$inputfile" | \
+			grep --color=never -E "[[:blank:]]verb[[:blank:]]*$" "$inputfile" | \
 			cut -f1,2 | grep "^[^#]" > dictverbs.txt
+			# 2020-03-21: For enver 3.5.0 or newer versions:
+			grep --color=never -E "^\[you\] " "$inputfile" >> dictverbs.txt  
 
 			echo -e "\nDIE VOKABELDATEI IST URHEBERRECHTLICH GESCHUETZT, DARF NUR PRIVAT GENUTZT WERDEN:\n"
 			head -n12 "$inputfile" | grep '^#'
